08a20dce46c1bbd7ba78648c70a237785d1b8759
Cesar Valle <cevpino@gmail.com>
2021-07-11 22:40:07 -0600

    Se incluye la logica en JS

--- descuentos.js
+++ descuentos.js
@@ -1,1 +1,25 @@
 console.log("Practica con porcentajes y descuentos")
+
+// const precioOriginal = 120;
+
+// const descuento = 18;
+
+// const porcentajePrecioDescuento = 100 - descuento;
+
+// const precioConDescuento = (precioOriginal * porcentajePrecioDescuento) / 100;
+
+// console.log({
+//    precioOriginal,
+//    descuento,
+//    porcentajePrecioDescuento,
+//    precioConDescuento
+// })
+
+function calcularPrecioConDescuento(price, discount){
+    const porcPriceDiscount = 100 - discount;
+
+    const priceWithDiscount = (price * porcPriceDiscount) / 100;
+    
+    return priceWithDiscount;
+}
+calcularPrecioConDescuento();
